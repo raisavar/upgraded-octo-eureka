357e1817e187aad902ddcdf2f8c719f24c90a425
raisavar <raisavar@users.noreply.github.com>
2025-10-13 00:43:35 +0300

    style: add input sanitization

--- src/api.js
+++ src/api.js
@@ -374,3 +374,30 @@ class API {
 
 // Update 95
 module.exports = API;
+
+
+// API module for EurekaPro
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 97
+module.exports = API;
