1f22ac1cb9ae4c49d47fb7150c23f8338f7e628f
raisavar <raisavar@users.noreply.github.com>
2025-12-07 14:56:10 +0200

    test: code refactoring

--- src/api.js
+++ src/api.js
@@ -104,3 +104,30 @@ class API {
 
 // Update 34
 module.exports = API;
+
+
+// API module for EurekaPro
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 37
+module.exports = API;
